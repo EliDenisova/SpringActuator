bce86b967d0685e1a61a18b62ded28a0226b1354
Elena <0koshko0@gmail.com>
2024-03-10 14:27:41 +0300

    first

---
+++ src/main/java/ru/java/SpringActuator/repository/TaskRepository.java
@@ -0,0 +1,20 @@
+package ru.java.SpringActuator.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import ru.java.SpringActuator.model.Task;
+import ru.java.SpringActuator.model.TaskStatus;
+
+
+import java.util.List;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long> {
+
+    /**
+     * Получить список задач по их статусу
+     * @param status
+     * @return
+     */
+    List<Task> findByStatus(TaskStatus status);
+}
---
+++ src/test/java/ru/java/SpringActuator/SpringActuatorApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.java.SpringActuator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringActuatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
